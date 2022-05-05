17609a9fe9500c400215807bb1bca1069c2a40d6
kmacdon7 <kmacdon7@nd.edu>
2022-05-05 12:01:56 -0400

    Fault Protection on Card

--- Components/Card.js
+++ Components/Card.js
@@ -8,13 +8,15 @@ const Card = ({ card }) => (
     activeOpacity={1}
     style={styles.card}
   >
+    
     <Image
       style={styles.image}
       source={card.photo}
       resizeMode="cover"
     />
+  
     <View style={styles.photoDescriptionContainer}>
-      <View style={{flexDirection: 'row'}}>
+      <View style={{flexDirection: 'row', flexWrap:"wrap"}}>
         {card.hobbies.map((hobby) => (
           <TouchableOpacity style={[styles.tag, {backgroundColor: '#9fa4d0'}]}>
             <Text style={{color: 'white', fontFamily: 'Avenir', fontSize: 12}}>&nbsp;{hobby}</Text>
@@ -23,10 +25,10 @@ const Card = ({ card }) => (
         <Text style={{marginBottom: 15}}>{'\n'}</Text>
       </View>
       <Text style={styles.text}>
-        {`${card.name}`}
+        {`${card.name ? card.name : "Out of Profiles"}`}
       </Text>
       <Text style={styles.bio}>
-        {`${card.bio}`}
+        {`${card.bio ? card.bio : "We'll have more soon!"}`}
       </Text>
     
         </View>
